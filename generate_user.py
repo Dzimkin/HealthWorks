4f776fb1dd5a073b3a67335f9a75ee4595926b38
Dmitry <guzich_d@bk.ru>
2023-04-17 18:12:41 +0300

    Add generate_user

--- generate_user.py
+++ generate_user.py
@@ -4,12 +4,31 @@ import random
 def generate_name():
     name = ''
     for x in range(5):
-        name += random.choice(list('abcdefghigklmnopqrstuvyxwzABCDEFGHIGKLMNOPQRSTUVYXWZ1234567890'))
+        name += random.choice(list('abcdefghigklmnopqrstuvyxwzABCDEFGHIGKLMNOPQRSTUVYXWZ'))
     return name
 
 
 def generate_source():
     source = ''
     for x in range(5):
-        source += random.choice(list('abcdefghigklmnopqrstuvyxwzABCDEFGHIGKLMNOPQRSTUVYXWZ1234567890'))
+        source += random.choice(list('abcdefghigklmnopqrstuvyxwzABCDEFGHIGKLMNOPQRSTUVYXWZ'))
     return source
+
+def generate_date():
+    date = ''
+    for x in range(6):
+        date += random.choice(list('1234567890'))
+    return date
+
+def generate_program():
+    program = ''
+    for x in range(5):
+        program += random.choice(list('abcdefghigklmnopqrstuvyxwzABCDEFGHIGKLMNOPQRSTUVYXWZ'))
+    return program
+
+def generate_number():
+    number = ''
+    for x in range(10):
+        number += random.choice(list('1234567890'))
+    return number
+
